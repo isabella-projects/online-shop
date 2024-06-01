583514d2783bc98ab907e1efbf34111f8d50ffd1
isabella-projects <damyan4oy@gmail.com>
2024-06-01 12:26:27 +0200

    Serving HTML pages for the existing routes Helper function for getting the absolute path of our project

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -13,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>404</h1><br><p>Page Not Found</p>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(PORT, () => {
