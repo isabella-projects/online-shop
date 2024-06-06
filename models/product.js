4a304bc3d202f0598ac56b1501205cbfc51a566f
isabella-projects <damyan4oy@gmail.com>
2024-06-06 14:27:24 +0200

    Storing product data Displaying, editing and deleting products

--- models/product.js
+++ models/product.js
@@ -15,8 +15,11 @@ const getProducsFromFile = (callback) => {
 };
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
